994b3063de101b0de1a0efa8ad2899383be92f04
Cristark <mihaimita02@gmail.com>
2023-02-12 16:24:20 +0100

    libreria terminada

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mmita <mmita@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 10:26:39 by mmita             #+#    #+#             */
-/*   Updated: 2023/02/06 10:27:06 by mmita            ###   ########.fr       */
+/*   Updated: 2023/02/12 16:23:47 by mmita            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,12 @@
 #  define BUFFER_SIZE 42
 # endif
 
-
+char	*get_next_line(int fd);
+char	*ft_read_memo(int fd, char *memo);
+char	*ft_return_line(char *memo);
+char	*ft_update_memo(char *memo);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
 
 #endif
