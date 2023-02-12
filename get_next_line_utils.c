7be199cac6a136aceb43337c8006120d3051d50d
Cristark <mihaimita02@gmail.com>
2023-02-12 16:07:44 +0100

    utils termiandos

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,65 @@
 /*   By: mmita <mmita@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 10:23:51 by mmita             #+#    #+#             */
-/*   Updated: 2023/02/06 10:23:52 by mmita            ###   ########.fr       */
+/*   Updated: 2023/02/12 16:07:12 by mmita            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc (sizeof(*str) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!s1 || !s2 || !str)
+	{
+		return (NULL);
+	}
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)(s + i));
+	else
+		return (NULL);
+}
